7cde8a1b164641f748f67d8778d91e8783f8e534
Kimberley Gray <followkim@gmail.com>
2025-06-24 22:09:39 -0700

    added play again wrapper

--- numberGuess.r
+++ numberGuess.r
@@ -1,21 +1,25 @@
 
+playAgain = "yes"
+max = 100
+while ((substr(playAgain, 1, 1)) == 'y') {
 
-answer <- sample(1:100, 1)
-guess <- 0
-guesses <- 0
+	answer <- sample(1:max, 1)
+	guess <- 0
+	guesses <- 0
 
+	print("Guess a number between 1 and 100")
 
-print("Guess a number between 1 and 100")
-while (guess != answer) {
-	guessStr <- readline()
-	guess <- as.integer(guessStr)
-	guesses = guesses + 1
-
-	if (guess < answer) { 
-		print(paste(guessStr, "is too low!"))
-	} else if (guess > answer) { 
-		print(paste(guessStr, "is too high!"))
-	} else {
-		print(paste("You got it in", guesses, "guesses!"))
+	while (guess != answer) {
+		guesses = guesses + 1
+		guessStr <- readline("-> ")
+		guess <- as.integer(guessStr)
+		if (guess < answer) { 
+			print(paste(guessStr, "is too low!"))
+		} else if (guess > answer) { 
+			print(paste(guessStr, "is too high!"))
+		} else {
+			print(paste("You got it in", guesses, "guesses!"))
+		}
 	}
+	playAgain <- readline("Play again? [Y/N]")
 }

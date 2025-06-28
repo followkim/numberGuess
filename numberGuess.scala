c54eebc60cded6f9d1ce77de71ccb1bfc7925070
Kimberley Gray <followkim@gmail.com>
2025-06-28 15:10:37 -0700

    Update numberGuess.scala
    working now!

--- numberGuess.scala
+++ numberGuess.scala
@@ -1,23 +1,33 @@
 import scala.io.StdIn._
 
+//> using scala 3.7.1
 
-val rand = new scala.util.Random
+@main
+def numberGuess(): Unit =
 
-var answer = rand.nextInt(100)+1
-var guess = 0
-var guesses = 0
+        val rand = new scala.util.Random
+        var playAgain = "yes"
 
-println(s"$answer")
 
-while (guess != answer) {
-  var guessStr = readLine("Prompt :>")
-  guess = guessStr.toIntOption.getOrElse(-1)
-  guesses += 1
-  
-  if (guess < answer) {
-    println(s"$guess is too low!")
-  } else if (guess > answer) {
-    println(s"$guess is too high!")
-  }
-}
-println(s"You got it in $guesses guesses!")
+        while (playAgain.startsWith("y")) {
+                var answer = rand.nextInt(100)+1
+                var guess = 0
+                var guesses = 0
+
+                while (guess != answer) {
+                        var guessStr = readLine("Guess :>")
+                        guess = guessStr.toIntOption.getOrElse(-1)
+                        guesses += 1
+
+                        if (guess < 0) {
+                                println(s"Please inpoout a number!!")
+                        } else if (guess < answer) {
+                                println(s"$guess is too low!")
+                        } else if (guess > answer) {
+                                println(s"$guess is too high!")
+                        }
+                }
+                println(s"You got it in $guesses guesses!")
+                playAgain = readLine("Play again [y/n] :>")
+        }
+        println(s"Thank you for playing!")
